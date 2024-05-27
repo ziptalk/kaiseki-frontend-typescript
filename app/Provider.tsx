5c0c4ece49399aaf77f503c82074f935d0157200
gkfyr <harock050110@naver.com>
2024-05-27 18:25:59 +0900

    feat: Seif recommend added

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -11,7 +11,7 @@ import {
   seiDevnet,
 } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-// import { CreateConnector } from "@rainbow-me/rainbowkit/dist/wallets/Wallet";
+import { CreateConnector } from "../node_modules/@rainbow-me/rainbowkit/dist/wallets/Wallet";
 import {
   getDefaultConfig,
   getDefaultWallets,
@@ -33,12 +33,12 @@ const { wallets } = getDefaultWallets();
 
 const config = getDefaultConfig({
   // wallets: [...wallets],
-  // wallets: [
-  //   {
-  //     groupName: "Recommended",
-  //     wallets: [seifWallet, metaMaskWallet, walletConnectWallet],
-  //   },
-  // ],
+  wallets: [
+    {
+      groupName: "Recommended",
+      wallets: [seifWallet, metaMaskWallet, walletConnectWallet],
+    },
+  ],
   appName: "My RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
   chains: [mainnet, polygon, optimism, arbitrum, base, seiDevnet],
@@ -49,68 +49,68 @@ const queryClient = new QueryClient();
 
 // config와 queryClient를 Provider 바깥으로 호이스팅!!
 
-// function getExplicitInjectedProvider() {
-//   if (typeof window === "undefined") return;
-//   if (window.ethereum && window.ethereum["__seif"]) {
-//     return window.ethereum;
-//   }
+function getExplicitInjectedProvider() {
+  if (typeof window === "undefined") return;
+  if (window.ethereum && window.ethereum["__seif"]) {
+    return window.ethereum;
+  }
 
-//   if ((window as any)["__seif"]) {
-//     return (window as any)["__seif"];
-//   }
+  if ((window as any)["__seif"]) {
+    return (window as any)["__seif"];
+  }
 
-//   return undefined;
-// }
+  return undefined;
+}
 
-// function createInjectedConnector(provider?: any): CreateConnector {
-//   return (walletDetails: WalletDetailsParams) => {
-//     // Create the injected configuration object conditionally based on the provider.
-//     const injectedConfig = provider
-//       ? {
-//           target: () => ({
-//             id: walletDetails.rkDetails.id,
-//             name: walletDetails.rkDetails.name,
-//             provider,
-//           }),
-//         }
-//       : {};
+function createInjectedConnector(provider?: any): CreateConnector {
+  return (walletDetails: WalletDetailsParams) => {
+    // Create the injected configuration object conditionally based on the provider.
+    const injectedConfig = provider
+      ? {
+          target: () => ({
+            id: walletDetails.rkDetails.id,
+            name: walletDetails.rkDetails.name,
+            provider,
+          }),
+        }
+      : {};
 
-//     return createConnector((config) => ({
-//       // Spread the injectedConfig object, which may be empty or contain the target function
-//       ...injected(injectedConfig)(config),
-//       ...walletDetails,
-//     }));
-//   };
-// }
+    return createConnector((config) => ({
+      // Spread the injectedConfig object, which may be empty or contain the target function
+      ...injected(injectedConfig)(config),
+      ...walletDetails,
+    }));
+  };
+}
 
-// function seifWallet(): Wallet {
-//   const injectedProvider = getExplicitInjectedProvider();
-//   return {
-//     id: "seif",
-//     name: "Seif",
-//     installed: !!injectedProvider,
-//     iconUrl: "https://wallet-edge.vercel.app/static/SeifIcon.svg",
-//     iconBackground: "#fff",
-//     downloadUrls: {
-//       chrome:
-//         "https://chromewebstore.google.com/detail/seif/albakdmmdafeafbehmcpoejenbeojejl",
-//     },
-//     createConnector: createInjectedConnector(injectedProvider),
-//     rdns: "com.passkeywallet.seif",
-//   };
-// }
+function seifWallet(): Wallet {
+  const injectedProvider = getExplicitInjectedProvider();
+  return {
+    id: "seif",
+    name: "Seif",
+    installed: !!injectedProvider,
+    iconUrl: "https://wallet-edge.vercel.app/static/SeifIcon.svg",
+    iconBackground: "#fff",
+    downloadUrls: {
+      chrome:
+        "https://chromewebstore.google.com/detail/seif/albakdmmdafeafbehmcpoejenbeojejl",
+    },
+    createConnector: createInjectedConnector(injectedProvider),
+    rdns: "com.passkeywallet.seif",
+  };
+}
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  // useEffect(() => {
-  //   window.addEventListener("eip6963:announceProvider", (event: any) => {
-  //     const announcedProvider = {
-  //       ...event.detail,
-  //       connected: false,
-  //       accounts: [],
-  //     };
-  //     console.log(announcedProvider);
-  //   });
-  // }, []);
+  useEffect(() => {
+    window.addEventListener("eip6963:announceProvider", (event: any) => {
+      const announcedProvider = {
+        ...event.detail,
+        connected: false,
+        accounts: [],
+      };
+      console.log(announcedProvider);
+    });
+  }, []);
 
   return (
     <RecoilRoot>
