1f68c8d1b192ca2ba7ac6d0aa3f352e9ddde4654
PROMLEE <donghoon099@naver.com>
2024-09-24 18:32:12 +0900

    Optimize handling of rotating image animation in SlotLayout component

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -129,7 +129,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
 
   function isMobileDevice() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent,
+      navigator && navigator.userAgent,
     );
   }
 
