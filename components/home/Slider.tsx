ffd45c6862bae42639b3553721b0300bbe79cf61
PROMLEE <donghoon099@naver.com>
2024-10-08 14:42:06 +0900

    Optimize package dependencies and add Slider component

---
+++ components/home/Slider.tsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useRef, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Link from "next/link";
+import { RWATokenCard } from "./RwaTokenCard";
+import { SlotSection } from "./SlotSection";
+import { MainTitle } from "../common/MainTitle";
+import { Raffle } from "@/utils/apis/apis";
+import { RaffleResponse, TokenResponse } from "@/utils/apis/type";
+import { useAccount } from "wagmi";
+import Arrow from "@/public/icons/leftArrowCircle.svg";
+
+const SliderComp = () => {
+  const account = useAccount();
+  const [raffleData, setRaffleData] = useState<RaffleResponse | null>(null);
+  const [oldSlide, setOldSlide] = useState(0);
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide2, setActiveSlide2] = useState(0);
+  let sliderRef = useRef<Slider>(null);
+  useEffect(() => {
+    getRaffle();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getRaffle();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [raffleData]);
+
+  const getRaffle = async () => {
+    const response = await Raffle();
+    setRaffleData(response);
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    arrows: false,
+    beforeChange: (current: number, next: number) => {
+      setOldSlide(current);
+      setActiveSlide(next);
+    },
+    afterChange: (current: number) => setActiveSlide2(current),
+  };
+  return (
+    <div className="main-inner w-full px-9 md:h-[474px] md:w-[520px] md:px-14">
+      <Slider {...settings} className="h-full w-full" ref={sliderRef}>
+        {raffleData &&
+          raffleData.result &&
+          raffleData.result.tokens &&
+          raffleData.result.tokens.map(
+            (raffle: TokenResponse, index: number) => {
+              let href = "/raffle";
+              for (let i = 0; i < raffleData.winner.raffles.length; i++) {
+                if (
+                  raffleData.winner.raffles[i].tokenAddress === raffle.token &&
+                  raffleData.winner.raffles[i].winnerAddress === account.address
+                ) {
+                  href = "/raffle/check";
+                  return;
+                } else if (
+                  raffleData.winner.raffles[i].tokenAddress === raffle.token
+                ) {
+                  href = "/raffle/fail";
+                }
+              }
+              return (
+                <Link
+                  key={index}
+                  href={{
+                    pathname: href,
+                    query: raffle &&
+                      raffle && {
+                        cid: raffle.cid,
+                        rafflePrize: raffle.rafflePrize,
+                        token: raffle.token,
+                        name: raffle.name,
+                        symbol: raffle.symbol,
+                        creator: raffle.creator,
+                        description: raffle.description,
+                        startDate: raffle.startDate,
+                      },
+                  }}
+                >
+                  <div className="flex w-full flex-col items-center gap-5">
+                    <MainTitle
+                      title={
+                        raffle && raffle.token
+                          ? href === "/raffle/check" || href === "/raffle/fail"
+                            ? "Check the winner!"
+                            : "Raffle is ready!"
+                          : "Loading..."
+                      }
+                    />
+                    {raffle && (
+                      <>
+                        <RWATokenCard props={raffle} />
+                        <SlotSection cid={raffle.cid} />
+                      </>
+                    )}
+                  </div>
+                </Link>
+              );
+            },
+          )}
+        {/* {raffleData && raffleData.result.tokens.length} */}
+      </Slider>
+
+      {raffleData && raffleData.result.tokens && (
+        <div className="flex select-none items-center gap-3 md:gap-5">
+          <Arrow
+            fill={activeSlide + 1 > 1 ? "white" : "#AEAEAE"}
+            className="cursor-pointer"
+            onClick={() => {
+              if (activeSlide + 1 > 1) sliderRef.current?.slickPrev();
+            }}
+          />
+          <h1 className="text-sm text-white md:text-base">
+            {activeSlide + 1} / {raffleData.result.tokens.length || 0}
+          </h1>
+          <Arrow
+            fill={
+              activeSlide + 1 < raffleData.result.tokens.length
+                ? "white"
+                : "#AEAEAE"
+            }
+            className="rotate-180 transform cursor-pointer"
+            onClick={() => {
+              if (activeSlide + 1 < raffleData.result.tokens.length)
+                sliderRef.current?.slickNext();
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SliderComp;
--- layout/home/SlotLayout.tsx
+++ layout/home/SlotLayout.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
-import { RWATokenCard } from "@/components/home/RwaTokenCard";
-import { SlotSection } from "@/components/home/SlotSection";
-import Link from "next/link";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import Arrow from "@/public/icons/leftArrowCircle.svg";
-import { MainTitle } from "@/components/common/MainTitle";
 import Stick from "@/public/icons/stick.svg";
-import { Raffle } from "@/utils/apis/apis";
-import { RaffleResponse } from "@/utils/apis/type";
 import { useAccount } from "wagmi";
+import SliderComp from "@/components/home/Slider";
 
 const RotatingImage = () => {
   return (
@@ -31,184 +25,98 @@ const RotatingPCImage = ({ hovered }: { hovered: boolean }) => {
     </div>
   );
 };
+
 export const SlotLayout = () => {
   const router = useRouter();
-  const account = useAccount();
   const [isHovered, setIsHovered] = useState(false);
-  const [raffleData, setRaffleData] = useState<RaffleResponse | null>(null);
-  const [page, setPage] = useState(0);
-  const [totalPage, setTotalPage] = useState(0);
-  const [stickColor, setStickColor] = useState(true);
-  const [href, setHref] = useState("/");
+  // useEffect(() => {
+  //   if (raffleData && raffleData.result && raffleData.result.tokens) {
+  //     setTotalPage(raffleData.result.tokens.length);
+  //   }
+  // }, [raffleData]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setStickColor(!stickColor);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [stickColor]);
+  // useEffect(() => {
+  //   if (
+  //     page &&
+  //     raffleData &&
+  //     raffleData.result &&
+  //     raffleData.result.tokens &&
+  //     raffleData.result.tokens.length
+  //   ) {
+  //     setHref("/raffle");
+  //     for (
+  //       let i = 0;
+  //       raffleData.winner.raffles && i < raffleData.winner.raffles.length;
+  //       i++
+  //     ) {
+  //       if (
+  //         raffleData.winner.raffles[i].tokenAddress ===
+  //           raffleData.result.tokens[page - 1].token &&
+  //         raffleData.winner.raffles[i].winnerAddress === account.address
+  //       ) {
+  //         setHref("/raffle/check");
+  //         return;
+  //       } else if (
+  //         raffleData.winner.raffles[i].tokenAddress ===
+  //         raffleData.result.tokens[page - 1].token
+  //       ) {
+  //         setHref("/raffle/fail");
+  //       }
+  //     }
+  //   } else {
+  //     setHref("/");
+  //   }
+  // }, [page, raffleData]);
 
-  const getRaffle = async () => {
-    const response = await Raffle();
-    setRaffleData(response);
-  };
+  // useEffect(() => {
+  //   if (totalPage) setPage(1);
+  // }, [totalPage]);
 
-  useEffect(() => {
-    if (raffleData && raffleData.result && raffleData.result.tokens) {
-      setTotalPage(raffleData.result.tokens.length);
-    }
-  }, [raffleData]);
-
-  useEffect(() => {
-    if (
-      page &&
-      raffleData &&
-      raffleData.result &&
-      raffleData.result.tokens &&
-      raffleData.result.tokens.length
-    ) {
-      setHref("/raffle");
-      for (
-        let i = 0;
-        raffleData.winner.raffles && i < raffleData.winner.raffles.length;
-        i++
-      ) {
-        if (
-          raffleData.winner.raffles[i].tokenAddress ===
-            raffleData.result.tokens[page - 1].token &&
-          raffleData.winner.raffles[i].winnerAddress === account.address
-        ) {
-          setHref("/raffle/check");
-          return;
-        } else if (
-          raffleData.winner.raffles[i].tokenAddress ===
-          raffleData.result.tokens[page - 1].token
-        ) {
-          setHref("/raffle/fail");
-        }
-      }
-    } else {
-      setHref("/");
-    }
-  }, [page, raffleData]);
-
-  useEffect(() => {
-    if (totalPage) setPage(1);
-  }, [totalPage]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getRaffle();
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [raffleData]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (totalPage === 0) return;
-      if (page === totalPage) {
-        setPage(1);
-      } else {
-        setPage(page + 1);
-      }
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [page]);
-
-  useEffect(() => {
-    getRaffle();
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (totalPage === 0) return;
+  //     if (page === totalPage) {
+  //       setPage(1);
+  //     } else {
+  //       setPage(page + 1);
+  //     }
+  //   }, 5000);
+  //   return () => clearInterval(interval);
+  // }, [page]);
 
   return (
-    <div className="main w-full flex-col p-5 pb-0 md:h-[534px] md:w-[1150px] md:flex-row md:pb-5">
-      <div className="main-inner w-full px-9 md:h-[474px] md:w-[520px] md:px-14">
+    <>
+      <div className="main w-full flex-col p-5 pb-0 md:h-[534px] md:w-[1150px] md:flex-row md:pb-5">
+        {/* <div className="main-inner w-full px-9 md:h-[474px] md:w-[520px] md:px-14"> */}
         {/* raffle section */}
-        <Link
-          href={{
-            pathname: href,
-            query: raffleData &&
-              raffleData.result.tokens &&
-              raffleData.result.tokens[page - 1] && {
-                cid: raffleData.result.tokens[page - 1].cid,
-                rafflePrize: raffleData.result.tokens[page - 1].rafflePrize,
-                token: raffleData.result.tokens[page - 1].token,
-                name: raffleData.result.tokens[page - 1].name,
-                symbol: raffleData.result.tokens[page - 1].symbol,
-                creator: raffleData.result.tokens[page - 1].creator,
-                description: raffleData.result.tokens[page - 1].description,
-                startDate: raffleData.result.tokens[page - 1].startDate,
-              },
-          }}
-          className="flex w-full flex-col items-center gap-5"
-        >
-          <MainTitle
-            title={
-              raffleData && raffleData.result.tokens
-                ? href === "/raffle/check" || href === "/raffle/fail"
-                  ? "Check the winner!"
-                  : "Raffle is ready!"
-                : "Loading..."
-            }
-          />
-          {raffleData &&
-            raffleData.result.tokens &&
-            raffleData.result.tokens[page - 1] && (
-              <>
-                <RWATokenCard props={raffleData.result.tokens[page - 1]} />
-                <SlotSection cid={raffleData.result.tokens[page - 1].cid} />
-              </>
-            )}
-        </Link>
 
+        <SliderComp />
         {/* move to prev/next raffle */}
-        {raffleData &&
-          raffleData.result.tokens &&
-          raffleData.result.tokens[page - 1] && (
-            <div className="flex items-center gap-3 md:gap-5">
-              <Arrow
-                fill={page > 1 ? "white" : "#AEAEAE"}
-                className="cursor-pointer"
-                onClick={() => {
-                  if (page > 1) setPage(page - 1);
-                }}
-              />
-              <h1 className="text-sm text-white md:text-base">
-                {page} / {totalPage || 0}
-              </h1>
-              <Arrow
-                fill={page < totalPage ? "white" : "#AEAEAE"}
-                className="rotate-180 transform cursor-pointer"
-                onClick={() => {
-                  if (page < totalPage) setPage(page + 1);
-                }}
-              />
-            </div>
-          )}
-      </div>
-
-      {/* create new coin */}
-      <div
-        className="create-default hidden h-[293px] w-[201px] cursor-pointer md:flex"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        onClick={() => router.push("/create")}
-      >
-        <h1 className="create-title">create new coin</h1>
-        <div className="flex h-36 w-32 items-center justify-center rounded-2xl border-2 border-[#A58C08] bg-[#0E0E0E]">
-          <RotatingPCImage hovered={isHovered} />
+        {/* </div> */}
+        {/* create new coin */}
+        <div
+          className="create-default hidden h-[293px] w-[201px] cursor-pointer md:flex"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => router.push("/create")}
+        >
+          <h1 className="create-title">create new coin</h1>
+          <div className="flex h-36 w-32 items-center justify-center rounded-2xl border-2 border-[#A58C08] bg-[#0E0E0E]">
+            <RotatingPCImage hovered={isHovered} />
+          </div>
+        </div>
+        <div
+          className="relative mt-10 flex w-full cursor-pointer flex-col items-center md:hidden"
+          onClick={() => router.push("/create")}
+        >
+          <div className="trapezoid-icon absolute top-[10px] rounded-full" />
+          {/* <Stick className={` fill-[#FF2626]`} /> */}
+          <RotatingImage />
+          <img src="images/rectangle-create.png" alt="" className="w-full" />
+          <div className="gradiant-create-box h-14 w-full" />
+          <div className="create-title absolute bottom-4">create new coin</div>
         </div>
       </div>
-      <div
-        className="relative mt-10 flex w-full cursor-pointer flex-col items-center md:hidden"
-        onClick={() => router.push("/create")}
-      >
-        <div className="trapezoid-icon absolute top-[10px] rounded-full" />
-        {/* <Stick className={` fill-[#FF2626]`} /> */}
-        <RotatingImage />
-        <img src="images/rectangle-create.png" alt="" className="w-full" />
-        <div className="gradiant-create-box h-14 w-full" />
-        <div className="create-title absolute bottom-4">create new coin</div>
-      </div>
-    </div>
+    </>
   );
 };
