ada260d648368505acee48d9a3ad1c692c5cbfaa
PROMLEE <donghoon099@naver.com>
2024-10-08 14:50:36 +0900

    Optimize autoplay speed in Slider component

--- components/home/Slider.tsx
+++ components/home/Slider.tsx
@@ -41,7 +41,7 @@ const SliderComp = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 8000,
+    autoplaySpeed: 5000,
     arrows: false,
     beforeChange: (current: number, next: number) => {
       setOldSlide(current);
