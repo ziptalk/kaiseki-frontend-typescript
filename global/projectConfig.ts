8c1fd3d3061509e9a83f227b79d6211f8191d9a5
PROMLEE <donghoon099@naver.com>
2025-04-09 15:43:53 +0900

    fix: update SERVER_ENDPOINT to new API URL for project configuration

--- global/projectConfig.ts
+++ global/projectConfig.ts
@@ -1,3 +1,3 @@
-export const SERVER_ENDPOINT = "https://api.kairaage.xyz";
+export const SERVER_ENDPOINT = "https://api.kaiseki.fun";
 export const PROJECT_CHAIN_ID = 1001;
 export const RESERVE_SYMBOL = "KAIROS";
