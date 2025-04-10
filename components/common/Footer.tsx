fe3779f9a1874565875c244d4b21b293b5ea0f8d
PROMLEE <donghoon099@naver.com>
2025-04-10 14:46:49 +0900

    style: update footer text for clarity and consistency

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="flex items-center justify-between px-5 pb-2 md:flex md:h-[134px] md:px-40">
-      <div className="whitespace-pre text-sm text-[#AEAEAE]">
-        Kaiseki | contact@blockwavelabs.io{"\n"}Kaiseki Inc.All rights reserved
+      <div className="text-sm whitespace-pre text-[#AEAEAE]">
+        Kaiseki | Kaiseki Inc.All rights reserved
       </div>
       <div />
     </div>
